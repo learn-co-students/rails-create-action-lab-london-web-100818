97f8b33224f2a53d2ee8dfc65ad99997b38738f9
ju-zp <jhuntington@tutanota.com>
2018-11-07 14:24:51 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,24 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    #byebug
+    if params.keys.include?("student")
+      @student = Student.new
+      @student.first_name = params[:student][:first_name]
+      @student.last_name = params[:student][:last_name]
+      @student.save
+    else
+      @student = Student.new
+      @student.first_name = params[:first_name]
+      @student.last_name = params[:last_name]
+      @student.save
+    end
+    redirect_to @student
+    
   end
 
 end
